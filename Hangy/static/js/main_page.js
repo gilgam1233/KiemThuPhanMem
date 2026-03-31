4fbe9812301559379bd9a95332ab96daa6f8349a
Website Admin <tranphuongvy03092005@gmail.com>
2026-03-31 18:47:57 +0700

    design base page

--- Hangy/static/js/main_page.js
+++ Hangy/static/js/main_page.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // ===========================================
-    // 1. Chuyển màu header khi scroll
-    // ===========================================
+    // đổi màu header khi scroll
     const header = document.querySelector('.main-header');
     if (header) {
-        // Hiệu ứng đổi màu khi cuộn
         window.addEventListener('scroll', () => {
             if (window.scrollY > 50) {
                 header.classList.add('scrolled');
